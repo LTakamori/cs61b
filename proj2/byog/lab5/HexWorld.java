2036e62b86c9c7a2b0bbccdc42771389a5415006
LTakamori <925951065@qq.com>
2020-10-27 18:33:18 +0800

    begin lab5

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -6,10 +6,26 @@ import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
 
+import javax.swing.text.Position;
 import java.util.Random;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+
+    /** this method is used to draw a Hexagon at Position with length(length must be greater than 2) */
+    public static void addHexagon(TETile[][] world, int length, Position p, TETile T){
+        if (length < 2){
+            //cant be less than 2
+        }
+
+
+    }
+
+    public static void drawTesselation(TETile[][] world, int length, Position p){
+    }
+
+    public static void main(int argc, String[] args){
+    }
 }
