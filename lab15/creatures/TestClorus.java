fe356953a8caa15f6dcfc2098a87f81a4f08bdb7
LTakamori <925951065@qq.com>
2020-11-08 16:45:09 +0800

    implement TestClorus

--- lab15/creatures/TestClorus.java
+++ lab15/creatures/TestClorus.java
@@ -1,4 +1,38 @@
 package creatures;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.HashMap;
+import java.awt.Color;
+import huglife.Direction;
+import huglife.Action;
+import huglife.Occupant;
+import huglife.Impassible;
+import huglife.Empty;
+
+/** Tests the plip class
+ *  @authr FIXME
+ */
 
 public class TestClorus {
+
+    /* Replace with the magic word given in lab.
+     * If you are submitting early, just put in "early" */
+    public static final String MAGIC_WORD = "";
+
+    @Test
+    public void testBasics() {
+        Clorus c = new Clorus(2);
+        assertEquals(2, c.energy(), 0.01);
+        assertEquals(new Color(32, 0, 231), p.color());
+    }
+
+    @Test
+    public void testReplicate() {
+        Clorus c = new Clorus(2);
+        assertNotSame(c, c.replicate());
+    }
+
+    public static void main(String[] args) {
+        System.exit(jh61b.junit.textui.runClasses(TestPlip.class));
+    }
 }
